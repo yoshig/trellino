81af3b82132a59b6ddc98d729dd4f137ec57e7b7
App Academy Student <student@appacademy.io>
2014-03-14 09:37:01 -0700

    starting trellino

---
+++ app/assets/javascripts/trellino.js
@@ -0,0 +1,13 @@
+window.Trellino = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Trellino.initialize();
+});
